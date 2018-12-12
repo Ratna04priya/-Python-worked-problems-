564ae6feca10d7818b2d06519552d53a0004c4c7
Ratna Priya <37251540+Ratna04priya@users.noreply.github.com>
2018-12-12 19:42:44 +0530

    Create re_match_search.py

---
+++ prob_-_soln/re_match_search.py
@@ -0,0 +1,26 @@
+# Comparison of match and search in re
+# Using group() , group(1), group(2)
+
+import re
+
+line = "Cats are smarter than dogs"
+
+#match
+matchObj = re.match( r'(.*) are (.*?) .*', line, re.M|re.I)
+
+if matchObj:
+   print "matchObj.group() : ", matchObj.group()
+   print "matchObj.group(1) : ", matchObj.group(1)
+   print "matchObj.group(2) : ", matchObj.group(2)
+else:
+   print "No match!!"
+
+#Search
+searchObj = re.search( r'(.*) are (.*?) .*', line, re.M|re.I)
+
+if searchObj:
+   print "searchObj.group() : ", searchObj.group()
+   print "searchObj.group(1) : ", searchObj.group(1)
+   print "searchObj.group(2) : ", searchObj.group(2)
+else:
+   print "Nothing found!!"
